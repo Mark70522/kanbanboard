d1b8aa6ec27ee391ea0ec29cfdbc5f08d19040d3
Mark70522 <1165132458@qq.com>
2025-06-21 20:45:36 +0800

    vite package

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: './',          // ← 新增：所有资源都按相对路径引用
   plugins: [vue()],
   resolve: {
     alias: {
